b3c3697c6929632f71bd32571eb39bde8c37ced1
Luis <lgd26308@gmail.com>
2022-02-27 22:12:41 -0700

    integrated password encryption using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,49 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 // name, email, photo, password, passwordConfirm
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    require: [true, 'Please tells us your name'],
+    required: [true, 'Please tells us your name'],
   },
   email: {
     type: String,
     required: [true, 'Please Provide your email'],
     unique: true,
     lowercase: true,
-    validator: [validator.isEmail, 'Please provide a valid email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
   },
   photo: {
     type: String,
   },
   password: {
     type: String,
-    require: [true, 'Please provide a password'],
+    required: [true, 'Please provide a password'],
     minlength: 8,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on SAVE!!!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
