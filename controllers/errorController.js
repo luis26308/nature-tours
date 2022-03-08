6e08b0dad095634057a2f9d00c29212dfbd5d85d
Luis <lgd26308@gmail.com>
2022-03-08 10:38:15 -0700

    updated error handling for invalid tokens

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -17,6 +17,9 @@ const handleValidationError = (err) => {
   return new AppError(message, 400);
 };
 
+const handleJWTError = () =>
+  new AppError('Invalid token, Please log in again!', 401);
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -57,6 +60,7 @@ module.exports = (err, req, res, next) => {
     if (err.name === 'CastError') err = handleCastErrorDB(err);
     if (err.code === 11000) err = handleDuplicateFieldsDB(err);
     if (err.name === 'ValidationError') err = handleValidationError(err);
+    if (err.name === 'JsonWebTokenError') err = handleJWTError(err);
 
     sendErrorProd(err, res);
   }
